f16ded5ec07fc92deb6a3b7006063a75ac2b035e
Tero Yrjölä <tero.yrjola@lindorff.com>
2017-07-14 14:50:36 +0300

    Update tests

--- GeneticAlgrorithmTest/Tests/GeneticallyCloseCharacterTests.cs
+++ GeneticAlgrorithmTest/Tests/GeneticallyCloseCharacterTests.cs
@@ -17,14 +17,15 @@ namespace GeneticAlgorithmTest.Tests
             Assert.That(testRng.Next(10, 20), Is.EqualTo(50));
         }
 
-        [TestCase(132, 'b', 'c')]
-        [TestCase(130, 'b', 'B')]
-        [TestCase(128, 'b', 'a')]
-        [TestCase(98, 'b', 'b')]
-        [TestCase(132, '~', 'A')]
-        [TestCase(128, 'A', '~')]
+        //In Ascii-mode, 96-99 give the neighbours and 100-101 switch case
+        [TestCase(96, 'b', 'a')]
+        [TestCase(98, 'b', 'c')]
+        [TestCase(100, 'b', 'B')]
+        [TestCase(96, ' ', '~')]
+        [TestCase(100, '!', '!')]
         public void GeneticallyCloseAsciiCharacterMappedCorrectly(int AsciiCode, char originalCharacter, char expectedResult)
         {
+            Helpers.AllowUpperCase = true;
             Helpers.AllAsciiCharacters = true;
             ArrangeRngToReturn(AsciiCode);
 
@@ -45,9 +46,9 @@ namespace GeneticAlgorithmTest.Tests
             Assert.That(Helpers.Near(c1, c2), Is.EqualTo(expected));
         }
 
-        [TestCase('a', 'b', true)]
-        [TestCase('a', 'Z', true)]
-        [TestCase('A', ' ', true)]
+        [TestCase('a', 'A', true)]
+        [TestCase('z', 'A', true)]
+        [TestCase('a', ' ', true)]
         [TestCase('a', 'c', false)]
         [TestCase('a', 'B', false)]
         public void GeneticallyNearUpperCaseTests(char c1, char c2, bool expected)
